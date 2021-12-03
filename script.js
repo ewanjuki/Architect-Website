6e0d282b29e86c823a964a9f1dfdc11da639d581
Edwin Wanjuki <edwinwanjuki@gmail.com>
2021-12-03 05:03:19 +0300

    create & style contact & footer section

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ document.querySelector('.hamburger-menu')
         const container = document.querySelector('.container');
         container.classList.toggle('change');
     });
+
+document.querySelector('.scroll-btn')
+    .addEventListener('click', () => {         
+        document.querySelector('html')
+            .style.scrollBehavior = 'smooth';
+
+        setTimeout(() => {
+            document.querySelector('html')
+                .style.scrollBehavior = 'unset';
+        }, 1000);
+    });
