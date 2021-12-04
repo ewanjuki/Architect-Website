79e8a8ab18ab25364d4155778e3abef5f7e3d6ec
Edwin Wanjuki <edwinwanjuki@gmail.com>
2021-12-04 04:49:18 +0300

    loading spinner

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+window.onload = () => {
+    setTimeout(() => {
+        document.querySelector('body').classList.add('display');
+    }, 4000);    
+};
+
 document.querySelector('.hamburger-menu')
     .addEventListener('click', () => {
         const container = document.querySelector('.container');
